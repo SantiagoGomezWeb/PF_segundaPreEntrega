a5fbdcef7d23b8afb7b453c62759e49b6a216e81
Santiago Gomez <santiago.daniel.gomez@gmail.com>
2023-05-29 20:08:51 -0300

    1er commit

---
+++ src/public/js/sendCart.js
@@ -0,0 +1,41 @@
+const products = document.getElementsByClassName('product');
+const iconCart = document.getElementById('number')
+
+const arrayProducts = Array.from(products);
+
+
+arrayProducts.forEach(product => {
+    product.addEventListener('click', () => {
+        const stock = Number(product.getAttribute('data-value'))
+        Swal.fire({
+            title: 'Ingrese la Cantidad',
+            input: 'text',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            cancelButtonText: 'Cancelar',
+            confirmButtonText: 'Confirmar',
+        }).then(response => {
+            console.log(response);
+            if (stock > Number(response.value)) {
+                Swal.fire({
+                    title: 'Product added successfully',
+                    text: `ID: ${product.id} - Quantity: ${response.value}`,
+                    icon: 'success',
+
+                })
+                fetch('http://localhost:8080/products', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ _id: product.id, quantity: Number(response.value) }),
+                }
+                )
+                
+            }
+        })
+        console.log(cart);
+    })
+});
---
+++ src/routers/products.router.js
@@ -0,0 +1,165 @@
+import { Router } from 'express';
+// import ProductManager from '../DAO/fileSystem/ProductManager.js';
+import ProductManager from '../DAO/mongo/managers/products.js';
+const routerP = Router();
+
+const pm = new ProductManager();
+
+
+routerP.get('/', async (request, response) => {
+    try {
+        let { limit, page, sort, category } = request.query
+        console.log(request.originalUrl);
+
+        const options = {
+            page: Number(page) || 1,
+            limit: Number(limit) || 10,
+            sort: { price: Number(sort) }
+        };
+
+        if (!(options.sort.price in [-1, 1])) {
+            delete options.sort
+        }
+
+
+        const links = (products) => {
+            let prevLink;
+            let nextLink;
+            if (request.originalUrl.includes('page')) {
+                prevLink = products.hasPrevPage ? request.originalUrl.replace(`page=${products.page}`, `page=${products.prevPage}`) : null;
+                nextLink = products.hasNextPage ? request.originalUrl.replace(`page=${products.page}`, `page=${products.nextPage}`) : null;
+                return { prevLink, nextLink };
+            }
+            if (!request.originalUrl.includes('?')) {
+                prevLink = products.hasPrevPage ? request.originalUrl.concat(`?page=${products.prevPage}`) : null;
+                nextLink = products.hasNextPage ? request.originalUrl.concat(`?page=${products.nextPage}`) : null;
+                return { prevLink, nextLink };
+            }
+            prevLink = products.hasPrevPage ? request.originalUrl.concat(`&page=${products.prevPage}`) : null;
+            nextLink = products.hasNextPage ? request.originalUrl.concat(`&page=${products.nextPage}`) : null;
+            return { prevLink, nextLink };
+
+        }
+
+        const categories = await pm.categories()
+
+        const result = categories.some(categ => categ === category)
+        if (result) {
+
+            const products = await pm.getProducts({ category }, options);
+            const { prevLink, nextLink } = links(products);
+            const { totalPages, prevPage, nextPage, hasNextPage, hasPrevPage, docs } = products
+            return response.status(200).send({ status: 'success', payload: docs, totalPages, prevPage, nextPage, hasNextPage, hasPrevPage, prevLink, nextLink });
+        }
+
+        const products = await pm.getProducts({}, options);
+        console.log(products, 'Product');
+        const { totalPages, prevPage, nextPage, hasNextPage, hasPrevPage, docs } = products
+        const { prevLink, nextLink } = links(products);
+        return response.status(200).send({ status: 'success', payload: docs, totalPages, prevPage, nextPage, hasNextPage, hasPrevPage, prevLink, nextLink });
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+routerP.get('/:pid', async (request, response) => {
+    try {
+        const { pid } = request.params
+
+        const result = await pm.getProductById(pid)
+        console.log(result, 'resultado');
+        if (result.message) return response.status(404).send({ message: `ID: ${pid} not found` })
+        return response.status(200).send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+routerP.post('/', async (request, response) => {
+    try {
+        const product = request.body
+        const {
+            title,
+            description,
+            price,
+            code,
+            stock,
+            status,
+            category,
+            thumbnails
+        } = product
+
+        const checkProduct = Object.values({
+            title,
+            description,
+            price,
+            code,
+            stock,
+            status,
+            category,
+            thumbnails
+        }).every(property => property)
+
+        if (!checkProduct) return response
+            .status(400)
+            .send({ message: "The product doesn't have all the properties" });
+
+        if (!(typeof title === 'string' &&
+            typeof description === 'string' &&
+            typeof price === 'number' &&
+            typeof code === 'string' &&
+            typeof stock === 'number' &&
+            typeof status === 'boolean' &&
+            typeof category === 'string' &&
+            Array.isArray(thumbnails)))
+            return response.status(400).send({ message: 'type of property is not valid' })
+
+        if (price < 0 || stock < 0) return response
+            .status(400)
+            .send({ message: 'Product and stock cannot be values less than or equal to zero' });
+
+        const result = await pm.addProduct(product)
+        console.log(result)
+        if (result.code === 11000) return response
+            .status(400)
+            .send({ message: `E11000 duplicate key error collection: ecommerce.products dup key code: ${result.keyValue.code}` });
+
+        return response.status(201).send(result);
+    }
+    catch (err) {
+        console.log(err);
+
+    }
+});
+
+routerP.put('/:pid', async (request, response) => {
+    try {
+        const { pid } = request.params
+        const product = request.body
+
+        const result = await pm.updateProduct(pid, product);
+        console.log(result);
+        if (result.message) return response.status(404).send({ message: `ID: ${pid} not found` })
+
+        return response.status(200).send(`The product ${result.title} whit ID: ${result._id} was updated`);
+    }
+    catch (err) {
+        console.log(err);
+    };
+
+})
+
+routerP.delete('/:pid', async (request, response) => {
+    try {
+        const { pid } = request.params
+        const result = await pm.deleteProduct(pid)
+        if (!result) return response.status(404).send({ message: `ID: ${pid} not found` })
+
+        return response.status(200).send({ message: `ID: ${pid} was deleted` });
+
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+export default routerP;
